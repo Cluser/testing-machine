b8899d38cd1e3b3503558fb64020191a31c1c9d4
Cluser <mrbfilipiak@gmail.com>
2022-04-11 13:55:16 +0200

    Correct data read from plc

--- server/plc/general.py
+++ server/plc/general.py
@@ -2,6 +2,8 @@ import snap7
 import time
 from db import general as db
 
+from plc.data import station
+
 class Plc():
     def __init__(self) -> None:
         self.client = snap7.client.Client()
@@ -21,18 +23,29 @@ class Plc():
 
     def get_data(self):
         start = time.time()
-        data = self.client.db_read(99, 0, 260)
-        # bool = snap7.util.get_bool(data, 0, 0)
-        bool = self.get_bool()
-        int = snap7.util.get_int(data, 2)
-        real = snap7.util.get_real(data, 4)
-        string = snap7.util.get_string(data, 8, 255)
-        end = time.time()
-        print('[0.0]: ', bool, '[2.0]: ', int, '[4.0]:', real, '[8.0]:', string, '[Read time]:', end - start, 's')
-
-        values = { "bool": bool, "int": int, 'real': real, 'dateTime': end }
+        data = self.client.db_read(199, 0, 28)
         # db.values.insert_one(values)
-        return values
+
+        station.lifebit = snap7.util.get_bool(data, 0, 0)
+        for x in range(20):
+               station.alarm[x] = snap7.util.get_bool(data, 2, x) 
+
+
+        station.module[0].id = snap7.util.get_int(data, 6)
+        station.module[0].process[0].id = snap7.util.get_int(data, 8)
+
+        station.module[0].process[0].status = snap7.util.get_int(data, 14)
+        station.module[0].process[0].spindle_no = snap7.util.get_int(data, 16)
+        station.module[0].process[0].time_left = snap7.util.get_int(data, 18)
+        station.module[0].process[0].spindle_velocity = snap7.util.get_int(data, 20)
+        station.module[0].process[0].motor_velocity = snap7.util.get_int(data, 22)
+        station.module[0].process[0].motor_temperature = snap7.util.get_int(data, 24)
+        station.module[0].process[0].outside_temperature = snap7.util.get_int(data, 26)
+
+        end = time.time()
+        print('station.module[0].process[0].spindle_velocity: ', station.module[0].process[0].spindle_velocity, end - start, 's')
+
+        return 5
         
 
 
