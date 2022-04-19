6ec922efdca7133219208917cf66a3b6ede2f69f
Cluser <mrbfilipiak@gmail.com>
2022-04-19 11:39:02 +0200

    Menu tab highlight by route

--- client/src/app/station/menu/menu.component.ts
+++ client/src/app/station/menu/menu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectUrl } from "src/app/core/store/router";
+import { IAppState } from "src/app/core/store/states";
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   selector: "app-menu",
@@ -6,7 +10,29 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit {
-  constructor() {}
+  private subscriptions = new Subscription();
+  public selectedUrl: string = "";
 
-  ngOnInit(): void {}
+  constructor(private store: Store<IAppState>) {}
+
+  ngOnInit(): void {
+    this.initSubscriptions();
+  }
+
+  ngOnDestroy(): void {
+    this.closeSubscriptions();
+  }
+
+  private initSubscriptions(): void {
+    const url$: Observable<string> = this.store.select(selectUrl);
+    this.subscriptions.add(url$.subscribe((url: string) => this.onUrlUpdate(url)));
+  }
+
+  private closeSubscriptions(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private onUrlUpdate(url: string): void {
+    this.selectedUrl = url;
+  }
 }
