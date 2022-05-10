7b4448529b2ef8c74ae56d44f8cc2900956d7aa4
Cluser <mrbfilipiak@gmail.com>
2022-05-10 12:43:51 +0200

    Add module recipe selection

--- client/src/app/station/module/module-recipe/module-recipe.component.ts
+++ client/src/app/station/module/module-recipe/module-recipe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { IAppState } from "src/app/core/store/states";
+import { IAppState, IRecipeState } from "src/app/core/store/states";
+import { getRecipes, getRecipesData } from "src/app/core/store/recipe";
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   selector: "app-module-recipe",
@@ -8,9 +10,42 @@ import { IAppState } from "src/app/core/store/states";
   styleUrls: ["./module-recipe.component.scss"],
 })
 export class ModuleRecipeComponent implements OnInit {
+  private subscriptions = new Subscription();
+  public recipeState: IRecipeState = { recipeEdit: {}, recipe: [] };
+
   constructor(private store: Store<IAppState>) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.initSubscriptions();
+    this.getRecipes();
+  }
 
-  ngOnDestroy() {}
+  ngOnDestroy(): void {
+    this.closeSubscriptions();
+  }
+
+  private initSubscriptions() {
+    const recipes$: Observable<IRecipeState> = this.store.select(getRecipesData);
+    this.subscriptions.add(
+      recipes$.subscribe((recipeState: IRecipeState) => {
+        this.recipeState = recipeState;
+      })
+    );
+  }
+
+  private closeSubscriptions() {
+    this.subscriptions.unsubscribe();
+  }
+
+  private getRecipes() {
+    this.store.dispatch(getRecipes());
+  }
+
+  public onRecipeChange(recipe: any): void {
+    // this.store.dispatch(changeEditRecipe({ recipeEdit: recipe }));
+  }
+
+  public compareFn(optionOne: any, optionTwo: any): boolean {
+    return optionOne?._id === optionTwo?._id;
+  }
 }
