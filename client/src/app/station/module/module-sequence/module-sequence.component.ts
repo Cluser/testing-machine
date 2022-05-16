5d8c896f99dacaa28686e971a4cfb894809bea39
Cluser <mrbfilipiak@gmail.com>
2022-05-16 14:41:45 +0200

    Add styling for buttons

--- client/src/app/station/module/module-sequence/module-sequence.component.ts
+++ client/src/app/station/module/module-sequence/module-sequence.component.ts
@@ -10,27 +10,18 @@ import { ColDef } from "ag-grid-community";
 })
 export class ModuleSequenceComponent implements OnInit {
   public selectedSequence: string = "grinding";
-  public columnDefs: ColDef[] = [
-    { field: "step" },
-    { field: "velocity" },
-    { field: "stepTime" },
-    { field: "temperature1" },
-    { field: "temperature2" },
-    { field: "temperatureLimit" },
-    { field: "oilFogTon" },
-    { field: "oilFogTof" },
-  ];
+  public columnDefs: ColDef[] = [{ field: "step" }, { field: "velocity" }, { field: "stepTime" }, { field: "oilFogTon" }, { field: "oilFogTof" }];
 
   public rowData = [
-    { step: 1, velocity: "10000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 2, velocity: "15000", stepTime: 20, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 4, velocity: "12000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 5, velocity: "10000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 6, velocity: "15000", stepTime: 20, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 7, velocity: "12000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 8, velocity: "10000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 9, velocity: "15000", stepTime: 20, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
-    { step: 10, velocity: "12000", stepTime: 10, temperature1: true, temperature2: true, temperatureLimit: 40, oilFogTon: 21, oilFogTof: 540 },
+    { step: 1, velocity: "10000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
+    { step: 2, velocity: "15000", stepTime: 20, oilFogTon: 21, oilFogTof: 540 },
+    { step: 4, velocity: "12000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
+    { step: 5, velocity: "10000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
+    { step: 6, velocity: "15000", stepTime: 20, oilFogTon: 21, oilFogTof: 540 },
+    { step: 7, velocity: "12000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
+    { step: 8, velocity: "10000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
+    { step: 9, velocity: "15000", stepTime: 20, oilFogTon: 21, oilFogTof: 540 },
+    { step: 10, velocity: "12000", stepTime: 10, oilFogTon: 21, oilFogTof: 540 },
   ];
   constructor(private store: Store<IAppState>) {}
 
