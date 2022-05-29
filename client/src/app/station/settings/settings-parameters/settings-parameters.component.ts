7fd7e6caa33d6dfe0e45ff70d99f7cd783b11a13
Cluser <mrbfilipiak@gmail.com>
2022-05-30 01:16:37 +0200

    Add save recipe modal

--- client/src/app/station/settings/settings-parameters/settings-parameters.component.ts
+++ client/src/app/station/settings/settings-parameters/settings-parameters.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
+import { SimpleModalService } from "ngx-simple-modal";
 import { Observable, Subscription } from "rxjs";
 import { changeRecipeName, changeRecipeTemperatureLimit, getRecipeEditName, getRecipeEditTemperatureLimit, saveRecipe } from "src/app/core/store/recipe";
 import { IAppState } from "src/app/core/store/states";
+import { ModalConfirmComponent } from "src/app/shared/modals/modal-confirm/modal-confirm.modal";
 
 @Component({
   selector: "app-settings-parameters",
@@ -15,7 +17,7 @@ export class SettingsParametersComponent implements OnInit {
   public name?: string;
   public temperatureLimit?: number;
 
-  constructor(private store: Store<IAppState>) {}
+  constructor(private store: Store<IAppState>, private simpleModalService: SimpleModalService) {}
 
   ngOnInit(): void {
     this.initSubscriptions();
@@ -56,6 +58,8 @@ export class SettingsParametersComponent implements OnInit {
   }
 
   public saveRecipe(): void {
-    this.store.dispatch(saveRecipe());
+    this.simpleModalService.addModal(ModalConfirmComponent, { title: "Zapisywanie referencji", message: "Czy na pewno chcesz zapisać referencję " + this.name + "?" }).subscribe((isConfirmed) => {
+      if (isConfirmed) this.store.dispatch(saveRecipe());
+    });
   }
 }
