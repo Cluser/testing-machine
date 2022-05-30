67219172cf20f6bbeb02c500a10a5a266aaefd19
Cluser <mrbfilipiak@gmail.com>
2022-05-30 16:02:07 +0200

    Recipe save confirmation on recipe change request

--- client/src/app/station/settings/settings-recipe/settings-recipe.component.ts
+++ client/src/app/station/settings/settings-recipe/settings-recipe.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { SimpleModalService } from "ngx-simple-modal";
-import { addRecipe, changeEditRecipe, getRecipes, getRecipesData, IRecipeState, removeRecipe } from "src/app/core/store/recipe";
+import { addRecipe, changeEditRecipe, getRecipes, getRecipesData, IRecipeState, removeRecipe, saveRecipe } from "src/app/core/store/recipe";
 import { IAppState } from "src/app/core/store/states";
 import { ModalConfirmComponent } from "src/app/shared/modals/modal-confirm/modal-confirm.modal";
 import { ModalAddRecipeComponent } from "./modal-add-recipe/modal-add-recipe.modal";
 import { Observable, Subscription } from "rxjs";
+import { IRecipe } from "src/app/shared/interfaces/IRecipe";
 
 @Component({
   selector: "app-settings-recipe",
@@ -15,7 +16,7 @@ import { Observable, Subscription } from "rxjs";
 export class SettingsRecipeComponent implements OnInit {
   private subscriptions = new Subscription();
   public recipeState: IRecipeState = { recipeEdit: {}, recipe: [] };
-  public selected: any;
+  public selected: Partial<IRecipe> = {};
 
   constructor(private store: Store<IAppState>, private simpleModalService: SimpleModalService) {}
 
@@ -33,6 +34,7 @@ export class SettingsRecipeComponent implements OnInit {
     this.subscriptions.add(
       recipes$.subscribe((recipeState: IRecipeState) => {
         this.recipeState = recipeState;
+        this.selected = { ...recipeState.recipeEdit };
       })
     );
   }
@@ -60,7 +62,12 @@ export class SettingsRecipeComponent implements OnInit {
   }
 
   public onRecipeChange(recipe: any): void {
-    this.store.dispatch(changeEditRecipe({ recipeEdit: recipe }));
+    this.simpleModalService
+      .addModal(ModalConfirmComponent, { title: "Zapisywanie referencji", message: "Czy chcesz zapisać obecną referencję o nazwie " + this.recipeState.recipeEdit.name + "?" })
+      .subscribe((isConfirmed) => {
+        if (isConfirmed) this.store.dispatch(saveRecipe());
+        this.store.dispatch(changeEditRecipe({ recipeEdit: recipe }));
+      });
   }
 
   public compareFn(optionOne: any, optionTwo: any): boolean {
