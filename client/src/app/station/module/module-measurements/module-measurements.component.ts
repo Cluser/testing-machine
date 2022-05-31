0471a507bc5f22208b8f02338ee44269d4028eb7
Cluser <mrbfilipiak@gmail.com>
2022-05-31 15:33:09 +0200

    Clear boilerplate code

--- client/src/app/station/module/module-measurements/module-measurements.component.ts
+++ client/src/app/station/module/module-measurements/module-measurements.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { IAppState, IModuleState } from "src/app/core/store/states";
-import { Observable, Subscription } from "rxjs";
 import { getModule } from "src/app/core/store/station";
 
 @Component({
@@ -10,25 +9,11 @@ import { getModule } from "src/app/core/store/station";
   styleUrls: ["./module-measurements.component.scss"],
 })
 export class ModuleMeasurementsComponent implements OnInit {
-  private subscriptions = new Subscription();
   public module: Partial<IModuleState> = {};
 
   constructor(private store: Store<IAppState>) {}
 
   ngOnInit() {
-    this.initSubscriptions();
-  }
-
-  ngOnDestroy() {
-    this.closeSubscriptions();
-  }
-
-  private initSubscriptions() {
-    const module$: Observable<IModuleState> = this.store.select(getModule);
-    this.subscriptions.add(module$.subscribe((module: IModuleState) => (this.module = module)));
-  }
-
-  private closeSubscriptions() {
-    this.subscriptions.unsubscribe();
+    this.store.select(getModule).subscribe((module: IModuleState) => (this.module = module));
   }
 }
--- client/src/app/station/module/module-recipe/module-recipe.component.ts
+++ client/src/app/station/module/module-recipe/module-recipe.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { IAppState, IRecipeState } from "src/app/core/store/states";
 import { getRecipes, getRecipesData, setRecipeActive } from "src/app/core/store/recipe";
-import { Observable, Subscription } from "rxjs";
 import { selectRouteParams } from "src/app/core/store/router";
 
 @Component({
@@ -11,40 +10,19 @@ import { selectRouteParams } from "src/app/core/store/router";
   styleUrls: ["./module-recipe.component.scss"],
 })
 export class ModuleRecipeComponent implements OnInit {
-  private subscriptions = new Subscription();
   public idModule: number = 0;
   public recipeState: IRecipeState = { recipeActive: [], recipeEdit: {}, recipe: [] };
 
   constructor(private store: Store<IAppState>) {}
 
   ngOnInit(): void {
-    this.initSubscriptions();
+    this.store.select(getRecipesData).subscribe((recipeState: IRecipeState) => {
+      this.recipeState = recipeState;
+    });
+    this.store.select(selectRouteParams).subscribe((params: any) => {
+      this.idModule = Number(params.id);
+    });
     this.getRecipes();
-  }
-
-  ngOnDestroy(): void {
-    this.closeSubscriptions();
-  }
-
-  private initSubscriptions() {
-    const recipes$: Observable<IRecipeState> = this.store.select(getRecipesData);
-    const routeParams$: Observable<any> = this.store.select(selectRouteParams);
-
-    this.subscriptions.add(
-      recipes$.subscribe((recipeState: IRecipeState) => {
-        this.recipeState = recipeState;
-      })
-    );
-
-    this.subscriptions.add(
-      routeParams$.subscribe((params: any) => {
-        this.idModule = Number(params.id);
-      })
-    );
-  }
-
-  private closeSubscriptions() {
-    this.subscriptions.unsubscribe();
   }
 
   private getRecipes() {
