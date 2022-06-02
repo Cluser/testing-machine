24c1adab0334410d41b7e8e8d54e001e197a05be
Cluser <mrbfilipiak@gmail.com>
2022-06-02 14:51:11 +0200

    Set records limit in charts results

--- client/src/app/core/store/chart/chart.reducer.ts
+++ client/src/app/core/store/chart/chart.reducer.ts
@@ -1,7 +1,9 @@
-import { createReducer, on, props } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { addChartData } from "./chart.actions";
 import { IChartState, initialChartState } from "./chart.state";
 import { ISeries } from "src/app/shared/interfaces/ILineChartResultSerie";
+
+const chartMaxRecords: number = 600;
 
 export const chartReducer = createReducer(
   initialChartState,
@@ -14,24 +16,24 @@ const onAddChartData = (state: IChartState, props: { chartData: ISeries[] }) => 
     {
       ...state.chart[0],
       results: [
-        { ...state.chart[0].results[0], series: state.chart[0].results[0].series?.concat(props.chartData[0]) },
-        { ...state.chart[0].results[1], series: state.chart[0].results[1].series?.concat(props.chartData[3]) },
+        { ...state.chart[0].results[0], series: state.chart[0].results[0].series?.slice(-chartMaxRecords).concat(props.chartData[0]) },
+        { ...state.chart[0].results[1], series: state.chart[0].results[1].series?.slice(-chartMaxRecords).concat(props.chartData[3]) },
       ],
     },
 
     {
       ...state.chart[1],
       results: [
-        { ...state.chart[1].results[0], series: state.chart[1].results[0].series?.concat(props.chartData[1]) },
-        { ...state.chart[1].results[1], series: state.chart[1].results[1].series?.concat(props.chartData[4]) },
+        { ...state.chart[1].results[0], series: state.chart[1].results[0].series?.slice(-chartMaxRecords).concat(props.chartData[1]) },
+        { ...state.chart[1].results[1], series: state.chart[1].results[1].series?.slice(-chartMaxRecords).concat(props.chartData[4]) },
       ],
     },
 
     {
       ...state.chart[2],
       results: [
-        { ...state.chart[2].results[0], series: state.chart[2].results[0].series?.concat(props.chartData[2]) },
-        { ...state.chart[2].results[1], series: state.chart[2].results[1].series?.concat(props.chartData[5]) },
+        { ...state.chart[2].results[0], series: state.chart[2].results[0].series?.slice(-chartMaxRecords).concat(props.chartData[2]) },
+        { ...state.chart[2].results[1], series: state.chart[2].results[1].series?.slice(-chartMaxRecords).concat(props.chartData[5]) },
       ],
     },
   ],
